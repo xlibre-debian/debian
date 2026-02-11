02c92bf131d069b96faa1811dc5072a1693cdd87
NexusSfan <nexussfan@duck.com>
2026-02-10 16:00:55 -0800

    Xserver 25.1.2

---
+++ include.sh
@@ -0,0 +1,7 @@
+reprepro -C stable includedeb main ~/XLibreDebianRemote/debian-packages-amd64/*.deb
+reprepro -C stable includedeb main ~/XLibreDebianRemote/debian-packages-arm64/*.deb
+reprepro -C testing includedeb main ~/XLibreDebianRemote/debian_testing-packages-amd64/*.deb
+reprepro -C testing includedeb main ~/XLibreDebianRemote/debian_testing-packages-arm64/*.deb
+reprepro export
+reprepro deleteunreferenced
+reprepro export

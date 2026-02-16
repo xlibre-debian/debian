fe8c4c48499ccbd8911d1f8e961ced5832aad31d
NexusSfan <nexussfan@duck.com>
2026-02-16 07:10:47 -0800

    AMDGPU 25.1.1

--- include.sh
+++ include.sh
@@ -2,6 +2,10 @@ reprepro -C stable includedeb main ~/XLibreDebianRemote/debian-packages-amd64/*.
 reprepro -C stable includedeb main ~/XLibreDebianRemote/debian-packages-arm64/*.deb
 reprepro -C testing includedeb main ~/XLibreDebianRemote/debian_testing-packages-amd64/*.deb
 reprepro -C testing includedeb main ~/XLibreDebianRemote/debian_testing-packages-arm64/*.deb
+reprepro -C stable includedeb main ~/Debian/Projects/nexussfan-keyring.deb
+reprepro -C testing includedeb main ~/Debian/Projects/nexussfan-keyring.deb
+reprepro -C stable includedeb main ~/Debian/Projects/xlibre-keyring.deb
+reprepro -C testing includedeb main ~/Debian/Projects/xlibre-keyring.deb
 reprepro export
 reprepro deleteunreferenced
 reprepro export
